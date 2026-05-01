8462f18d2fdf955f70d91360466fa1643a2a7bbb
Senuoy-16 <youn6197@gmail.com>
2026-05-02 00:54:31 +0100

    remove UI library and migrate to Tailwind-only components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import { Button, Header, Link, Text } from "@jamsr-ui/react";
 import Orb from "../components/Orb";
 import { Logo } from "../components/Logo";
 import Advantages from "../components/Advantages";
@@ -6,14 +5,24 @@ import Advantages from "../components/Advantages";
 function Home(){
     return (
     <div className="w-full h-[700px] z-0 relative">
-      <Header>
+      <div
+        className="
+          fixed top-10 left-1/2 -translate-x-1/2
+          flex h-[60px] w-[90%] lg:max-w-screen-lg
+          backdrop-blur-[10px]
+          rounded-full border border-black
+          bg-black text-white
+          justify-between items-center px-6
+          z-50
+        "
+      >
         <Logo />
         <div className="flex items-center gap-4">
-          <Link href="#">Home</Link>
-          <Link href="#">Start Quiz</Link>
-          <Link href="#">Join</Link>
+          <a className="text-white border-b-2 border-transparent hover:border-[#ffffffa3] hover:text-[#ffffffa3] transition-all duration-300" href="#">Home</a>
+          <a className="text-white border-b-2 border-transparent hover:border-[#ffffffa3] hover:text-[#ffffffa3] transition-all duration-300" href="#">Start Quiz</a>
+          <a className="text-white border-b-2 border-transparent hover:border-[#ffffffa3] hover:text-[#ffffffa3] transition-all duration-300" href="#">Join</a>
         </div>
-      </Header>
+      </div>
 
       <Orb
         hoverIntensity={2}
@@ -23,23 +32,36 @@ function Home(){
       />
 
       <div className="absolute top-0 left-0 flex items-center justify-center flex-col w-full h-full z-1 pointer-events-none text-center">
-        <Button
-          size="md"
-          startContent={<img src="/background.svg" height={15} width={15} />}
-          variant="outlined"
-          className="text-foreground text-white"
+        <button
+          className="
+            flex items-center gap-2
+            px-4 py-2
+            rounded-full
+            bg-black text-white
+            border border-black
+            transition-all duration-300
+            hover:bg-white hover:text-black
+          "
         >
+          <img src="/background.svg" height={15} width={15} />
           New Service
-        </Button>
-        <Text as="h1" variant="h1" className="text-5xl mt-4 max-w-[18ch] text-black">
+        </button>
+        <h1 className="text-5xl mt-4 max-w-[18ch] text-black font-semibold">
           Quiz Generator Live
-        </Text>
-        <Text as="h1" variant="paragraph" className="text-4xl mt-4 max-w-[18ch] text-black">
+        </h1>
+        <p className="text-4xl mt-4 max-w-[18ch] text-black">
           Chose Who You are?
-        </Text>
+        </p>
         <div className="flex items-center gap-4 mt-8">
-          <Button className="pointer-events-auto">professor</Button>
-          <Button variant="light" className="pointer-events-auto">Student</Button>
+          <div className="flex items-center gap-4 mt-8">
+            <button className="cursor-pointer md:w-40 w-32 pointer-events-auto bg-black text-white px-6 py-3 rounded-full border border-black transition-all duration-300 hover:bg-white hover:text-black">
+              professor
+            </button>
+
+            <button className="cursor-pointer md:w-40 w-32 pointer-events-auto bg-white text-black px-6 py-3 rounded-full border border-black transition-all duration-300 hover:bg-black hover:text-white">
+              Student
+            </button>
+          </div>
         </div>
       </div>
 
