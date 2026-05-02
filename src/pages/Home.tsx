52812406420701158e6fcdcc7d6eb261b31c0c56
Senuoy-16 <youn6197@gmail.com>
2026-05-02 01:11:41 +0100

    fixing styling of navbar and hero_section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ function Home(){
     <div className="w-full h-[700px] z-0 relative">
       <div
         className="
-          fixed top-10 left-1/2 -translate-x-1/2
+          relative top-10 left-1/2 -translate-x-1/2
           flex h-[60px] w-[90%] lg:max-w-screen-lg
           backdrop-blur-[10px]
           rounded-full border border-black
@@ -34,7 +34,7 @@ function Home(){
       <div className="absolute top-0 left-0 flex items-center justify-center flex-col w-full h-full z-1 pointer-events-none text-center">
         <button
           className="
-            flex items-center gap-2
+            flex items-center gap-2 mt-10 mb-4 md:mb-0 md:mt-0
             px-4 py-2
             rounded-full
             bg-black text-white
@@ -46,14 +46,14 @@ function Home(){
           <img src="/background.svg" height={15} width={15} />
           New Service
         </button>
-        <h1 className="text-5xl mt-4 max-w-[18ch] text-black font-semibold">
+        <h1 className="text-3xl md:text-5xl md:mt-6 max-w-[18ch] text-black font-semibold">
           Quiz Generator Live
         </h1>
-        <p className="text-4xl mt-4 max-w-[18ch] text-black">
+        <p  className="text-3xl md:text-4xl md:mt-4 max-w-[18ch] text-black">
           Chose Who You are?
         </p>
-        <div className="flex items-center gap-4 mt-8">
-          <div className="flex items-center gap-4 mt-8">
+        <div className="flex items-center gap-4 md:mt-2 mt-2">
+          <div className="flex items-center gap-4 md:mt-8 mt-2">
             <button className="cursor-pointer md:w-40 w-32 pointer-events-auto bg-black text-white px-6 py-3 rounded-full border border-black transition-all duration-300 hover:bg-white hover:text-black">
               professor
             </button>
