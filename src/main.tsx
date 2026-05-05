54ea3dc09ea900dd1087c1a73b27f3e2476ad3f8
rabiibanine <rbanine@gmail.com>
2026-05-05 23:24:05 +0100

    chore: modify phosphor globals

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+import { IconContext } from "@phosphor-icons/react";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <IconContext.Provider value={{ size: 20, weight: "regular" }}>
+        <App />
+      </IconContext.Provider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
