6653212487ece85737215cdc3197920a314ecd89
rabiibanine <rbanine@gmail.com>
2026-05-05 11:53:15 +0100

    chore: tailwind upgrade failed but changed some files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
@@ -69,4 +69,3 @@ export default {
   },
   plugins: [],
 };
-
