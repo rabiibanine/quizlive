91e673a4d27b93ff8a6fd4739759872c8f132f46
rabiibanine <rbanine@gmail.com>
2026-05-06 23:47:54 +0100

    chore: final adjustments

--- src/components/ToolBarButton.tsx
+++ src/components/ToolBarButton.tsx
@@ -14,11 +14,11 @@ export default function ToolBarButton({
   return (
     <button
       onClick={onClick}
-      className={`flex items-center gap-2 px-4 py-2 rounded-full text-md font-medium transition-all
+      className={`flex items-center gap-2 px-4 py-2 cursor-pointer rounded-full text-md font-medium transition-all active:scale-95 
         ${
           variant === "primary"
             ? "bg-zinc-800 text-white hover:bg-zinc-700"
-            : "text-zinc-600 hover:bg-zinc-100"
+            : "text-zinc-600 hover:bg-zinc-100 active:bg-zinc-200"
         }`}
     >
       {children}
