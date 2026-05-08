105cca5d88de09e73605a4fbe07134994e066a11
rabiibanine <rbanine@gmail.com>
2026-05-08 21:32:35 +0100

    fix: session requires all of quiz

--- src/services/sessionServices.ts
+++ src/services/sessionServices.ts
@@ -1,19 +1,15 @@
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 import { db } from "@/firebase/firebase";
-import type { Question } from "@/types/index";
+import type { Quiz } from "@/types/index";
 
-export async function launchSession(
-  quizId: string,
-  professorId: string,
-  quizQuestions: Question[]
-) {
+export async function launchSession(quizId: string, professorId: string, quiz: Quiz[]) {
   const quizCode = Math.random().toString(36).slice(2, 8).toUpperCase();
   const ref = await addDoc(collection(db, "sessions"), {
     quizCode,
     quizId,
     professorId,
     status: "waiting",
-    questions: quizQuestions,
+    quiz: quiz,
     currentQuestion: 0,
     students: [],
     createdAt: serverTimestamp(),
