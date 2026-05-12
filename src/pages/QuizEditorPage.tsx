fc902973c4d31b51eea237146c78945ebcbe5d7f
rabiibanine <rbanine@gmail.com>
2026-05-13 00:43:41 +0100

    chore: remove sliders on ai question number input

--- src/pages/QuizEditorPage.tsx
+++ src/pages/QuizEditorPage.tsx
@@ -280,7 +280,7 @@ export default function QuizEditorPage() {
                 max={50}
                 value={questionCountInput}
                 onChange={(event) => setQuestionCountInput(event.target.value)}
-                className="w-24 rounded-full border border-white/10 bg-white/5 px-3 py-2 text-sm text-white focus:outline-none focus:ring-2 focus:ring-purple-400/40"
+                className="w-24 rounded-full border border-white/10 bg-white/5 px-3 py-2 text-sm text-white focus:outline-none focus:ring-2 focus:ring-purple-400/40 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none "
               />
             </div>
             <button
